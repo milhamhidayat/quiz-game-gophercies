f83b55486289c802df781e86604bed8efcbd0a63
M Ilham Hidayat <ilham@cocowork.co>
2018-09-22 15:31:22 +0700

    Create quiz game

--- main.go
+++ main.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type Problem struct {
+	Q string
+	A string
+}
 
 func main() {
-	fmt.Println("ok")
+	csvFileName := flag.String("csv", "problem.csv", "a csv file in the format of 'question, answer'")
+	flag.Parse()
+	_ = csvFileName
+
+	file, err := os.Open(*csvFileName)
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open the CSV file: %s\n", *csvFileName))
+	}
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit("Failed to parse the provided CSV file")
+	}
+	fmt.Println(lines)
+	problems := parseLines(lines)
+
+	correct := 0
+	for i, p := range problems {
+		fmt.Printf("Problem #%d: %s = \n", i+1, p.Q)
+		var answer string
+		fmt.Scanf("%s\n", &answer)
+		if answer == p.A {
+			correct++
+		}
+	}
+	fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
+}
+
+func parseLines(lines [][]string) []Problem {
+	ret := make([]Problem, len(lines))
+	for i, line := range lines {
+		ret[i] = Problem{
+			Q: line[0],
+			A: strings.TrimSpace(line[1]),
+		}
+	}
+	return ret
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
 }
