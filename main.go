4f6d774edcd0461ade6bad74fa1c1c4b99db37ce
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2018-09-23 22:35:17 +0700

    Done add sanitazion and shuffle

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"encoding/csv"
+	"errors"
 	"flag"
 	"fmt"
 	"math/rand"
@@ -17,110 +18,77 @@ type Problem struct {
 }
 
 func main() {
-	// `csv` flag to choose csv file to load in command line
 	csvFileName := flag.String("csv", "problem.csv", "a csv file in the format of 'question, answer'")
-	// `limit` flag to set quiz time limit (in second)
 	timeLimit := flag.Int("limit", 30, "the time limit for the quiz in seconds")
-
 	randomize := flag.Bool("random", false, "randomize questions")
 
-	// parse flag dari argumen command line
 	flag.Parse()
 
-	// open file csv, reference csvFileName using pointer
-	file, err := os.Open(*csvFileName)
+	csvFile, err := loadCsv(*csvFileName)
 	if err != nil {
-		exit(fmt.Sprintf("Failed to open the CSV file: %s\n", *csvFileName))
+		exit(err)
 	}
-	// read csv file
-	r := csv.NewReader(file)
-	// reads all remaining record from r to end of file,
-	// to array 2 dimension (contain item from csv file)
-	lines, err := r.ReadAll()
 
+	csvQA, err := readCsv(csvFile)
 	if err != nil {
-		exit("Failed to parse the provided CSV file")
+		exit(err)
 	}
 
 	if *randomize {
-		lines = shuffle(lines)
+		csvQA = shuffle(csvQA)
 	}
 
-	// memasukkan
-	problems := parseLines(lines)
-
-	// NewTimer -> create a new timer
-	// Duration -> to enable create new timer if NewTimer time is already stored in variable
+	problems := parseLines(csvQA)
 	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
-
 	reader := bufio.NewReader(os.Stdin)
-
-	// counter to correct answer
 	correct := 0
-	// problemloop:
+	done := make(chan bool, 1)
 
-	// Loop and print question
-	for i, p := range problems {
-		// Print question
-		fmt.Printf("Problem #%d: %s = ", i+1, p.Q)
+	go func() {
+		for i, p := range problems {
+			fmt.Printf("Problem #%d: %s = ", i+1, p.Q)
 
-		// Create answer channel
-		answerCh := make(chan string)
-
-		// create go routine to accept answer & to not block timer
-		go func() {
-			// var answer string
-
-			// // get answer from command line
-			// fmt.Scanf("%s\n", &answer)
-			// fmt.Println(answer)
-			// answer = strings.Replace(answer, "\n", "", -1)
-			// answer = strings.ToLower(answer)
-			// answer = strings.TrimSpace(answer)
-
-			// if answer == p.A {
-			// 	fmt.Println("ok")
-			// }
-
-			// read and return a strin
 			answer, _ := reader.ReadString('\n')
-
-			// return answer to answser channel
-			answerCh <- answer
-		}()
-
-		// make goroutine wait on multiple operations
-		select {
-
-		// jika timer sudah habis
-		case <-timer.C:
-
-			// cetak hasil
-			fmt.Printf("\nYou scored %d out of %d.\n", correct, len(problems))
-
-			// kelular dari problem
-			return
-			// fmt.Println()
-			// break problemloop
-
-		// jika menerima answer
-		case answer := <-answerCh:
-			// jika answer = jawaban asli, tambah nilai correct
-			var newAnswer string
-			newAnswer = strings.Replace(answer, "\n", "", -1)
-			newAnswer = strings.ToLower(newAnswer)
-			newAnswer = strings.TrimSpace(newAnswer)
-			if newAnswer == strings.ToLower(p.A) {
+			answer = strings.TrimSpace(strings.ToLower(strings.Replace(answer, "\n", "", -1)))
+			if answer == strings.ToLower(p.A) {
 				correct++
 			}
-
-			// if answer == p.A {
-			// 	correct++
-			// }
 		}
+		done <- true
+	}()
 
+	select {
+	case <-done:
+		fmt.Printf("\nGood Job!")
+	case <-timer.C:
+		fmt.Printf("\nTime is up")
 	}
-	fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
+
+	fmt.Printf("\nYou scored %d out of %d.\n", correct, len(problems))
+}
+
+func loadCsv(CsvFile string) (*os.File, error) {
+	file, err := os.Open(CsvFile)
+
+	if err != nil {
+		errMsg := errors.New(fmt.Sprintf("failed to open the CSV file: %s", CsvFile))
+		return nil, errMsg
+	}
+
+	return file, nil
+}
+
+func readCsv(csvData *os.File) ([][]string, error) {
+	r := csv.NewReader(csvData)
+	csvQA, err := r.ReadAll()
+
+	if err != nil {
+		errMsg := errors.New(fmt.Sprintf("failed to read the provided CSV file"))
+		return nil, errMsg
+	}
+
+	return csvQA, nil
+
 }
 
 func shuffle(lines [][]string) [][]string {
@@ -137,25 +105,20 @@ func shuffle(lines [][]string) [][]string {
 }
 
 func parseLines(lines [][]string) []Problem {
-	// buat slice dengan tipa struct : Problem, dengan jumlah sesuai panjang array lines
 	ret := make([]Problem, len(lines))
 
-	// perulangan untuk mengisi slice ret dengan question dan problem
 	for i, line := range lines {
-		// isi nilai array dari index 0, dengan struct Problem
 		ret[i] = Problem{
-			// question(Q) diisi dengan indeks 0 dari arr line
 
 			Q: line[0],
-			// answer(A) diisi dengan indeks 1 dari arr line
 			A: strings.TrimSpace(line[1]),
 		}
 	}
+
 	return ret
 }
 
-// function for exit from the program
-func exit(msg string) {
+func exit(msg error) {
 	fmt.Println(msg)
 	os.Exit(1)
 }
